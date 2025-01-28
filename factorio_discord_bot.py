5a842dcf00e3aed06aa8e2c14fdb2c7c6b5a9d30
Michael Higgins <mdhiggins23@icloud.com>
2025-01-28 10:03:18 -0500

    fix missing await

--- factorio_discord_bot.py
+++ factorio_discord_bot.py
@@ -127,7 +127,7 @@ async def check_server_status():
 
     # Check if the last message in the channel is from the bot
     if not last_message:
-        async for message in channel.pins():
+        async for message in await channel.pins():
             if message.author == bot.user:
                 last_message = message
                 break

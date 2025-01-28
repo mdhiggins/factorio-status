de7b823f84b7f28a38457af34ecdd8e3f820addc
Michael Higgins <mdhiggins23@icloud.com>
2025-01-28 12:37:08 -0500

    Update factorio_discord_bot.py

--- factorio_discord_bot.py
+++ factorio_discord_bot.py
@@ -154,6 +154,7 @@ async def check_server_status():
 async def on_ready():
     logging.info(f"{client.user} has connected to Discord!")
     check_server_status.start()
+    await tree.sync()
 
 
 # Register the slash command
